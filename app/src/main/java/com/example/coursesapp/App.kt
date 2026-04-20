8e0392702f3d84598c148f4f308483f8e59a4df7
nikita <dedukhin.nickita@yandex.ru>
2026-04-20 18:27:08 +0300

    chore: add application class and root dagger component

---
+++ app/src/main/java/com/example/coursesapp/App.kt
@@ -0,0 +1,6 @@
+package com.example.coursesapp
+
+import android.app.Application
+
+class App : Application() {
+}
---
+++ app/src/main/java/com/example/coursesapp/di/AppComponent.kt
@@ -0,0 +1,7 @@
+package com.example.coursesapp.di
+
+import dagger.Component
+
+@Component
+interface AppComponent {
+}
